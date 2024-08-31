ee40ec76fca58540474e768fb2f7c5f6b290c8ed
SharkTitan <aram.ad.2k05@gmail.com>
2024-08-31 23:37:24 +0400

    Add files via upload

--- ひらがなカタカナ.js
+++ ひらがなカタカナ.js
@@ -1,5 +1,5 @@
 $(function(){
-    var hiragana = ["あ", "い", "う", "え", "お",
+    var hiragana = ["い", "う", "え", "お",
                     "か", "き", "く", "け", "こ",
                     "さ", "し", "す", "せ", "そ",
                     "た", "ち", "つ", "て", "と",
@@ -16,23 +16,80 @@ $(function(){
                     "ざ", "じ", "ず", "ぜ", "ぞ",
                     "だ", "ぢ", "づ", "で", "ど",
                     "ば", "び", "ぶ", "べ", "ぼ",
-                    "ぱ", "ぴ", "ぷ", "ぺ", "ぽ",
+                    "ぱ", "ぴ", "ぷ", "ぺ", "ぽ"
 
     ];
+
+    var katakana = ["イ", "ウ", "エ", "オ",
+                    "カ", "キ", "ク", "ケ", "コ",
+                    "サ", "シ", "ス", "セ", "ソ",
+                    "タ", "チ", "ツ", "テ", "ト",
+                    "ナ", "ニ", "ヌ", "ネ", "ノ",
+                    "ハ", "ヒ", "フ", "ヘ", "ホ",
+                    "マ", "ミ", "ム", "メ", "モ",
+                    "ヤ", "ユ","ヨ",
+                    "ラ", "リ", "ル", "レ", "ロ",
+                    "ワ", "ヲ", "ン",
+
+                        // dakuten
+
+                    "ガ", "ギ", "グ", "ゲ", "ゴ",
+                    "ザ", "ジ", "ズ", "ゼ", "ゾ",
+                    "ダ", "ヂ", "ヅ", "デ", "ド",
+                    "バ", "ビ", "ブ", "ベ", "ボ",
+                    "パ", "ピ", "プ", "ペ", "ポ"                
+    ]
+    var currentKana = hiragana;
     var currentIndex = 0;
     var intervalId;
     var startIsSet = false;
     var delayHira =  1.0;
     var realDelay = 1000;
     var fromDone = 0;
+    var hiraKataTrueFalse = true;
+    var currentDelay = 1000;
     
     
+    
+    // katakanaHiraganaOptions
+
+    
+    $("#hiraganaoption").css("color", "#0dff00").css("text-shadow", "0px 0px 20px #0dff00");
+    $("#hirakata").text("あ");
+
+    $("#hiraganaoption").on('click', function(){
+
+        if (hiraKataTrueFalse == true){
+            if (currentKana == katakana) {
+                currentKana = hiragana;
+                $("#hiraganaoption").css("color", "#0dff00").css("text-shadow", "0px 0px 20px #0dff00");
+                $("#katakanaoption").css("color", "#ff2f00").css("text-shadow", "none");
+                $("#hirakata").text("あ").css("font-size", "50%");
+            }
+        }
+    })
+
+    $("#katakanaoption").on('click', function(){
+
+        if(hiraKataTrueFalse == true){
+            if (currentKana == hiragana) {
+                currentKana = katakana;
+                $("#katakanaoption").css("color", "#0dff00").css("text-shadow", "0px 0px 20px #0dff00");;
+                $("#hiraganaoption").css("color", "#ff2f00").css("text-shadow", "none");
+                $("#hirakata").text("ア").css("font-size", "50%");
+            }
+        }
+    })
+
+
+    // practiceCode
+
 
     function changeHiraIND() {
-        $("#hirakata").text(hiragana[currentIndex]).css("font-size", "50%");
+        $("#hirakata").text(currentKana[currentIndex]).css("font-size", "50%");
         currentIndex++;
 
-        if (currentIndex >= hiragana.length) {
+        if (currentIndex > currentKana.length) {
             currentIndex = 0;
             fromDone++;
         }
@@ -40,20 +97,28 @@ $(function(){
         if (fromDone >= 1){
             clearInterval(intervalId);
             $("#hirakata").text("Done!").css("font-size", "40%");
+            hiraKataTrueFalse = true;
             intervalId = null;
         }
     };
 
     $("#startBUT").on('click', function(){
         if (startIsSet == false){
-            $("#hirakata").text("あ").css("font-size", "50%");
-            var randHira = hiragana.sort((a, b) => 0.5 - Math.random());
-            console.log(hiragana)
+            if (currentKana == hiragana) {
+                $("#hirakata").text("あ").css("font-size", "50%");
+            }
+            if (currentKana == katakana) {
+                $("#hirakata").text("ア").css("font-size", "50%");
+            }
+            var randHira = currentKana.sort((a, b) => 0.5 - Math.random());
+            console.log(currentKana)
         }
     });
 
     $("#startBUT").on('click', function(){
+        hiraKataTrueFalse = false;
         startIsSet = true;
+
         if (!intervalId) {
             fromDone = 0;
             intervalId = setInterval(changeHiraIND, realDelay);
@@ -61,8 +126,10 @@ $(function(){
     });
 
     $("#stopBUT").on('click', function(){
+        currentIndex = 0;
         clearInterval(intervalId);
         intervalId = null;
+        hiraKataTrueFalse = true;
         if (startIsSet == true) {
             $("#hirakata").text("How'r you doin?").css("font-size", "23%" )
             startIsSet = false;
@@ -76,8 +143,9 @@ $(function(){
     // +0.1
 
     $("#rightDelay").on('click', function(){
-        delayHira = delayHira + 0.1;
-        realDelay = realDelay + 100;
+        delayHira = delayHira + 0.5;
+        realDelay = realDelay + 500;
+        currentDelay = currentDelay + 500;
 
         if (delayHira > 2){
             delayHira = 2;
@@ -93,8 +161,9 @@ $(function(){
     // +0.5
 
     $("#centerDelayR").on('click', function(){
-        delayHira = delayHira + 0.5;
-        realDelay = realDelay + 500;
+        delayHira = delayHira + 0.1;
+        realDelay = realDelay + 100;
+        currentDelay = currentDelay + 100;
 
         if (delayHira > 2){
             delayHira = 2;
@@ -109,8 +178,9 @@ $(function(){
     // -0.5
 
     $("#centerDelayL").on('click', function(){
-        delayHira = delayHira - 0.5;
-        realDelay = realDelay - 500;
+        delayHira = delayHira - 0.1;
+        realDelay = realDelay - 100;
+        currentDelay = currentDelay - 100;
 
         if (delayHira < 0.5){
             delayHira = 0.5;
@@ -125,8 +195,9 @@ $(function(){
     // -0.1
 
     $("#leftDelay").on('click', function(){
-        delayHira = delayHira - 0.1;
-        realDelay = realDelay - 100;
+        delayHira = delayHira - 0.5;
+        realDelay = realDelay - 500;
+        currentDelay = currentDelay - 500;
 
         if (delayHira < 0.5){
             delayHira = 0.5;
